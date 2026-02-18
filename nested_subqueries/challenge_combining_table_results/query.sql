396ff1e0e895ed35b03b00844d4701a8dfec1ed2
sitiatiqa <>
2026-02-18 21:08:09 +0000

    Challenge: Combining Table Results

---
+++ nested_subqueries/challenge_combining_table_results/query.sql
@@ -0,0 +1,9 @@
+SELECT id, first_name, last_name
+FROM (
+    select employee_id as id, first_name, last_name
+    from employees
+    union
+    select contractor_id as id, first_name, last_name
+    from contractors
+  ) AS combined
+where id % 2 = 0;
